437981f5c51a4a337471436a70a5ef7f1c352c95
AlicjaMacutkiewicz <alicja.m.milena@gmail.com>
2026-04-14 21:07:29 +0200

    minor improvements to var model: updated README and TODO, added some logs to main for debugging, removed reading .out files in main

--- prediction_models/VAR_Model/src/main.py
+++ prediction_models/VAR_Model/src/main.py
@@ -1,24 +1,39 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.api import VAR
 from statsmodels.tsa.stattools import adfuller
 
 def convert_to_stationary(dataframe, col):
     # differencing the data to make the time series stationary
-    # and dropping the missing values created after differencing
-    dataframe[col] = dataframe[col].diff().dropna()
+    
+    print ("Differencing the column: {}".format(col))
+    series = dataframe[col]
+    diff1 = series.diff()
+    
+    if check_stationarity(diff1):
+        print ("{} is now stationary".format(col))
+        dataframe[col] = diff1
+        return
+    
+    print ("{} is not stationary after the first differentiation".format(col))
+    print ("Differencing {} again".format(col))
+    diff2 = diff1.diff()
 
-    # differencing the second time if the data is still stationary
-    # after the first transformation
-    if check_stationarity(dataframe[col].dropna())==False: # why do I have to put .dropna here sklsdjksjfdsflj
-        dataframe[col] = dataframe[col].diff().dropna()
+    if check_stationarity(diff2):
+        print ("{} is now stationary".format(col))
+        dataframe[col] = diff2
+        return
+    print ("{} is still not stationary after the second differentiation".format(col))
+    dataframe[col] = diff2
+    
     # TODO: for now the if the data is still not stationary after the
     # second transformation the code continues as if the time series
     # were stationary. Check whether there is a better way to ensure
     # stationarity without overdifferencing
 
 def check_stationarity(data_series):
+    data_series = data_series.dropna()
     result = adfuller(data_series)
     # the data is stationary if the p-value drawn from the
     # ADF test is less or equal 0.05
@@ -32,20 +47,16 @@ def check_stationarity(data_series):
 # The data from the third flight is the testing data
 
 training_sensors1 = pd.read_csv('../../../model_translator/src/output/flight_0_best_sensors.csv')
-training_flight1 = pd.read_csv('../../../model_translator/src/output/flight_0.out', header=None)
+training_sensors1.set_index("Time", inplace=True)
 
 # Uncomment to read all the data from files
-# TODO:  modify reading the .out files
 
-# training_sensors1.set_index("Time", inplace=True)
 # training_sensors2 = pd.read_csv('../../../model_translator/src/output/flight_1_best_sensors.csv')
-# training_flight2 = pd.read_csv('../../../model_translator/src/output/flight_1.out', header=None)
-
+# training_sensors2.set_index("Time", inplace=True)
 # test_sensors = pd.read_csv('../../../model_translator/src/output/flight_0_best_sensors.csv')
-# test_data = pd.read_csv('../../../model_translator/src/output/flight_2.out', header=None)
+# test_sensors.set_index("Time", inplace=True)
 
 # Checking if the data is stationary and converting it if its not
-# TODO: check all the data, work out how to interpret .out files
 
 for col in training_sensors1.columns:
     if check_stationarity(training_sensors1[col]) == False:
