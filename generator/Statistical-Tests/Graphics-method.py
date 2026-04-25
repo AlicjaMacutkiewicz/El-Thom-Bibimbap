36861fb6fbbf0cfc1467282ab7588bea1649c068
pawel <pawel.leczkowski2005@gmail.com>
2026-04-25 20:57:56 +0200

    Graphics-method.py implemented + Kullback-Leibler-divergence.py deleted because it doesn't have sense

--- generator/Statistical-Tests/Graphics-method.py
+++ generator/Statistical-Tests/Graphics-method.py
@@ -1,3 +1,5 @@
+# creates overlying graphs of each column
+
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -6,7 +8,7 @@ columns = ['Best_Acc_X', 'Best_Acc_Y', 'Best_Acc_Z', 'Best_AngVel_X',
        'Thrust', 'Mass', 'Position_X', 'Position_Y', 'Position_Z',
        'Acceleration_X', 'Acceleration_Y', 'Acceleration_Z']
 
-N = 11
+N = 12
 tests = []
 
 for i in range(N):
@@ -18,7 +20,7 @@ for col in columns:
 
     for i in range(N):
         p = tests[i][col]
-        ax.plot(p, alpha=(1 / N), label=f'flight_{i}')
+        ax.plot(p, alpha=(1 / N))
 
     ax.set_title(col)
     ax.legend()
